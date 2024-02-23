3f418fb20e8da1efe7b1a1b0c7c38cd5cd0c9472
Iamop18 <121235654+Iamop18@users.noreply.github.com>
2024-02-23 15:58:33 +0530

    Add files via upload

---
+++ demo.c
@@ -0,0 +1,14 @@
+#include<studio.h>
+#include<conio.h>
+
+void main()
+{
+ int h,b;
+ float area;
+ clrscr();
+ printf("Enter the height and the base of the triangle");
+ scanf("%d",&h<&b);
+ area=(b*h)/2;
+ printf("the area of triangle is=%f",area);
+ getch();
+}
